06c121688205c3c3b72a57e9e0c4c1d2f2e7f2f1
Jim Carroll <jim@dontcallme.com>
2018-09-02 18:27:09 -0400

    initial commit.

---
+++ src/main/groovy/gst.groovy
@@ -0,0 +1,79 @@
+import org.codehaus.groovy.control.CompilerConfiguration
+import org.freedesktop.gstreamer.Gst
+
+import com.jiminger.gstreamer.*
+import com.jiminger.gstreamer.util.*
+import com.jiminger.gstreamer.guard.GstScope
+
+import ai.kognition.config.SpringContextLoader
+import ai.kognition.video.controller.service.VideoFeedService
+
+def usage() {
+   println "groovy -cp classpath ${this.class.name} path-to-script"
+   System.exit 1
+}
+
+class MainScript {
+   private final VideoFeedService videoFeedService;
+
+   MainScript(VideoFeedService vfs) {
+      videoFeedService =  vfs;
+   }
+
+   def bin(Closure closure) {
+      def binManager = new BinManager()
+      def inlineDisplays = []
+
+      BinManager.metaClass.rtsp { String uri -> videoFeedService.rtspSource(binManager,uri) }
+      BinManager.metaClass.display { String title ->
+         def id = new InlineDisplay()
+         binManager.add(new BreakoutFilter("inline-display-" + title)
+               .connect(id))
+         inlineDisplays.add(id)
+      }
+      BinManager.metaClass.play {
+         def pipe = binManager.buildPipeline()
+
+         // Add the method "printDetails" to Pipeline
+         pipe.getClass().metaClass.printDetails { GstUtils.printDetails(pipe) }
+
+         pipe.play()
+         inlineDisplays.forEach({ it.stopOnClose(pipe, null) })
+         return pipe
+      }
+
+      closure.delegate = binManager
+      closure.run()
+
+      return binManager
+   }
+}
+
+new GstScope(this.class.name, args).withCloseable( { scope ->
+   SpringContextLoader.load([
+      "spring/property-wiring.xml",
+      "spring/kogvfs.xml"
+   ] as String[], ["spring/main.xml"] as String[]).withCloseable( { ctx ->
+      videoFeedService = ctx.getBean(VideoFeedService.class);
+
+      // if (args.length != 1) usage()
+
+      def compilerConfiguration = new CompilerConfiguration()
+      compilerConfiguration.scriptBaseClass = DelegatingScript.class.name
+
+      Binding binding = new Binding()
+
+      def shell = new GroovyShell(binding, compilerConfiguration)
+
+      binding.setProperty('args', args)
+
+      def dslScript = shell.parse( new File(args[0]).text)
+
+      dslScript.setDelegate(new MainScript(videoFeedService))
+      dslScript.run()
+
+      Gst.main();
+   } )
+})
+
+
